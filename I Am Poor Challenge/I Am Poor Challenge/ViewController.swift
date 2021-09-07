65013ab1778a2fec456d201dd798f1bd2efa1220
Steve Hong <ponoponong@gmail.com>
2021-09-07 20:51:27 +0900

    Simple Apps

---
+++ I Am Poor Challenge/I Am Poor Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor Challenge
+//
+//  Created by 홍성범 on 2021/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
