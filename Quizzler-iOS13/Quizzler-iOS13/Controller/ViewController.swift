3b2f7108841d2b0c4e96dca47791847189ff48e6
Steve Hong <ponoponong@gmail.com>
2021-09-24 14:57:28 +0900

    About Structures and MVC Design Pattern

--- Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
-
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var btnOption1: UIButton!
+    @IBOutlet weak var btnOption2: UIButton!
+    @IBOutlet weak var btnOption3: UIButton!
+    
     var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
@@ -24,27 +26,32 @@ class ViewController: UIViewController {
     }
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
-        let userAnswer = sender.currentTitle // True, False
+        let userAnswer = sender.currentTitle! // True, False
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
         
-        if quizBrain.checkAnswer(userAnswer!) {
+        if userGotItRight {
             sender.backgroundColor = UIColor.green
         } else {
             sender.backgroundColor = UIColor.red
         }
         
-        if questionNumber < quiz.count - 1 {
-            questionNumber += 1
-        } else {
-            questionNumber = 0
-        }
+        quizBrain.nextQuestion()
+        
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
     @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
-        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        questionLabel.text = quizBrain.getQuestionText()
+        
+        let options = quizBrain.getOptions()
+        btnOption1.setTitle(options[0], for: .normal)
+        btnOption2.setTitle(options[1], for: .normal)
+        btnOption3.setTitle(options[2], for: .normal)
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        btnOption1.backgroundColor = UIColor.clear
+        btnOption2.backgroundColor = UIColor.clear
+        btnOption3.backgroundColor = UIColor.clear
     }
 
 }
