aa437f6c70b6298e05f317cf4fae83fd24f527d8
Steve Hong <ponoponong@gmail.com>
2021-10-30 01:15:54 +0900

    Protocols, Delegates, Closures, URL, URLSession, Networking, JSON Decoding, Decoder Protocol, etc. (Deleted my Openweather API key)

---
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by 홍성범 on 2021/10/29.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
---
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 홍성범 on 2021/10/29.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid={MYAPPKEY}&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+    }
+}
