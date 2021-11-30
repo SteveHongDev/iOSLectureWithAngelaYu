25ed5dac4d2fb39eaf58daf8d4ec8823642d1639
Steve Hong <ponoponong@gmail.com>
2021-11-30 20:55:04 +0900

    Used Firebase to make Login, Logout, Register functions

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 홍성범 on 2021/11/30.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String
+}
